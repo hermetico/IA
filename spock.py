1b8aa53de20ac6606824976958f03df7c0a790b2
Hermetico <yojmarinv@gmail.com>
2013-09-21 20:01:07 +0200

    Reconstruido totalmente

--- spock.py
+++ spock.py
@@ -8,96 +8,116 @@
     del ordenador y el ganador.
     Cuando el jugador decide salir, se muestra el numero de victorias y derrotas
 
-    :creador por Juan Marín y Dennis Quitaquís
+    Los resultados de los enfrentamientos de guardan en una matriz, en la cual
+    el jugador ocupa las columnas y la maquina las filas. Las consultas a la
+    matriz, son entonces del tipo : [jugador][maquina] == ?
+        
+        == 1 gana jugador
+        == 2 empate
+        == 3 gana maquina
+
+
+    :creado por Juan Marín y Dennis Quitaquís
 """
-
-# spock.py
-# programa per jugar al joc Pedra paper estissores, spock, llangardaix
-
 
 from random import randint as rnd
 
-SALUDO = '''
+#: texto que se muestra al iniciar el juego
+SALUDO = """
     Welcome to Rock Paper Scissors Spock Lizard\n
     let's play!
-'''
-MANO = 'Select your play:'
+"""
 
-JUGADASALIR = 'Press -1 to exit'
-SALIR = '-1'
+#: texto que se muestra al solicitar la jugada
+SOLICITAR = 'Select your play:'
 
-playerStats = {
+#: valor para salir del juego
+SALIR = -1
+
+#: valor con el cual gana el jugador
+GANA_JUGADOR = 1
+
+#: valor con el cual gana la maquina
+GANA_MAQUINA = 3
+
+#: valor de empate
+EMPATE = 2
+
+#: jugadas disponibles
+JUGADAS = ['Rock', 'Paper', 'Scissor', 'Spock', 'Lizard']
+
+#: resultados de cada enfrentamiento
+RESULTADOS = [
+    [2, 3, 1, 2, 1],
+    [1, 2, 3, 1, 3],
+    [3, 1, 2, 3, 1],
+    [1, 3, 1, 2, 3],
+    [3, 1, 3, 1, 2]
+]
+    
+#: stadisticas del jugador a lo largo de las partidas
+estadisticas_jugador = {
     'win'     : 0,
     'lose'    : 0
 }
 
-hands = {
-    '0' : {
-        'name' : 'Rock',
-        'defeat' : [2, 4]
-    },
-    '1' : {
-        'name' : 'Paper',
-        'defeat' : [0, 3]
-    },
-    '2' : {
-        'name' : 'Scissors',
-        'defeat' : [1, 4]
-    },
-    '3' : {
-       'name' : 'Spock',
-        'defeat' : [0, 2]
-    },
-    '4' : {
-        'name' : 'Lizard',
-        'defeat' : [1, 3]
-    }
-}
-
-def playerHand():
-    print MANO
-    #mostramos los numeros que puede entrar
-    for key in sorted(hands.keys()):
-        print 'Press %s for play with %s' %(key, hands[key]['name'])
-
-    print JUGADASALIR
+def solicitar_jugada():
+    """Funcion que solicita la jugada y devuelve el valor como int"""
     
-    return raw_input(">> ")
+    print
+    print SOLICITAR
+    # mostramos las jugadas disponibles y el numero relacionado
+    for i in range(len(JUGADAS)):
+        print "Press %i for play with %s" %(i, JUGADAS[i])
+    
+    # mostramos el valor a pulsar para salir
+    print "Press %i for exit" %SALIR
+    
+    return input(">> ")
 
 
-def showResults():
-    print 'You have won %s games' % str(playerStats['win'])
-    print 'And you have lost %s games' %str(playerStats['lose'])
+def mostrar_estadisticas():
+    """Funcion que muestra las estadisticas del jugador"""
+
+    print
+    print 'You have won %i games' % estadisticas_jugador['win']
+    print 'And lost %s games' % estadisticas_jugador['lose']
+
 
 def main():
+    """Funcion principal del juego"""
+    #: variable que nos indica si continuamos jugando
+    continue_playing = True
+
     print
     print SALUDO
-    
-    while(True): 
-        print
-        player = playerHand()
-        computer = str(rnd(0,4))
-
-    
-        #comprobamos si tenemos que salir del juego
-        if player == '-1':
-            return None
-
-        if not player in hands.keys():
+    while continue_playing:
+        # solicitamos la mano del jugdor
+        jugador = solicitar_jugada()
+        # comprobamos si ha introducido el valor para salir
+        if jugador == SALIR:
+            continue_playing = False
+        # comprobamos que sea una mano valida
+        elif jugador < -1 or jugador > (len(JUGADAS) - 1):
             pass
-
-        elif player == computer: # draw
-            print 'Draw'
-
-        elif int(player) in hands[computer]['defeat']: # gana maquina
-            playerStats['lose'] += 1
-            print 'You lose : %s defeat %s ' %(hands[computer]['name'],hands[player]['name'])
-
-        else: # gana jugador
-            playerStats['win'] += 1
-            print 'You win : %s defeat %s ' %(hands[player]['name'], hands[computer]['name'])
+        else:
+            # calculamos jugada de la maquina
+            maquina = rnd(0, len(JUGADAS) - 1)
+            print "You play with %s and computer plays with %s" %(
+                JUGADAS[jugador], JUGADAS[maquina])
+            if RESULTADOS[jugador][maquina] == EMPATE:
+                print "Draw!"
+            else:
+                if RESULTADOS[jugador][maquina] == GANA_JUGADOR:
+                    print "You win because %s defeats %s" %(JUGADAS[jugador],
+                                                            JUGADAS[maquina])
+                    estadisticas_jugador['win'] += 1
+                else:
+                    print "You lose because %s not defeats %s" %(JUGADAS[jugador],
+                                                                JUGADAS[maquina])
+                    estadisticas_jugador['lose'] += 1
+    # mostramos las estadisticas
+    mostrar_estadisticas()
 
 
 main()
-showResults()
-
